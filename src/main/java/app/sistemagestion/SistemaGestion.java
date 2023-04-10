a4b0fbce561966a397040eb342f25080051a7961
Yosemar <yojecava@outlook.com>
2023-04-10 14:26:04 -0600

    first commit

---
+++ src/main/java/app/sistemagestion/SistemaGestion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.sistemagestion;
+
+/**
+ *
+ * @author yosemar
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
